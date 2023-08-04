56f7d64d29e9c10576ed637246864806eeb3f79b
Ruslan <ruslanosadtsia@gmail.com>
2023-08-04 21:13:20 +0300

    init commit

---
+++ app/src/main/java/com/finance/wisebudget/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.finance.wisebudget
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.finance.wisebudget.ui.theme.WiseBudgetTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WiseBudgetTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    WiseBudgetTheme {
+        Greeting("Android")
+    }
+}
---
+++ domain/src/main/java/com/finance/domain/MyClass.kt
@@ -0,0 +1,4 @@
+package com.finance.domain
+
+class MyClass {
+}
